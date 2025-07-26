2bc3848aadd35502e4b3395a9479825de67716d9
yousmakhayyam <yousmakhayyam@gmail.com>
2025-07-26 12:56:09 +0500

    change to main.tf commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+# VARIABLES
 variable "resource_group_name" {}
 variable "location" {}
 variable "acr_name" {}
@@ -19,11 +20,13 @@ variable "acr_admin_username" {}
 variable "acr_admin_password" {}
 variable "container_image" {}
 
+# RESOURCE GROUP
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# ACR
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.main.name
@@ -32,40 +35,38 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_app_service_plan" "asp" {
+# ✅ UPDATED App Service Plan
+resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
+# ✅ FIXED Key Vault (removed soft_delete_enabled)
 resource "azurerm_key_vault" "kv" {
   name                        = var.key_vault_name
   location                    = var.location
   resource_group_name         = azurerm_resource_group.main.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_enabled         = true
   purge_protection_enabled    = false
 }
 
+# Key Vault Secret
 resource "azurerm_key_vault_secret" "api_key" {
   name         = "EMAIL-API-KEY"
   value        = var.email_api_key
   key_vault_id = azurerm_key_vault.kv.id
 }
 
+# Web App for Containers
 resource "azurerm_linux_web_app" "app" {
   name                = var.web_app_name
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  service_plan_id     = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.asp.id
 
   identity {
     type = "SystemAssigned"
@@ -77,16 +78,19 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   app_settings = {
-    WEBSITES_PORT = "3000"
-    EMAIL_API_KEY = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.api_key.id})"
+    WEBSITES_PORT                    = "3000"
+    EMAIL_API_KEY                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.api_key.id})"
     DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
     DOCKER_REGISTRY_SERVER_USERNAME = var.acr_admin_username
     DOCKER_REGISTRY_SERVER_PASSWORD = var.acr_admin_password
   }
 
-  depends_on = [azurerm_key_vault_secret.api_key]
+  depends_on = [
+    azurerm_key_vault_secret.api_key
+  ]
 }
 
+# Key Vault Access Policy for Web App
 resource "azurerm_key_vault_access_policy" "app_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
