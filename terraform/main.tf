ffeea1b2e65acc6719b0aa64270e16dbeb69f96e
yousmakhayyam <yousmakhayyam@gmail.com>
2025-07-26 22:05:23 +0500

    rerun change to main.tf commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,65 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+#############################
+# 🔧 Variables Declaration  #
+#############################
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "acr_name" {
+  type        = string
+  description = "Azure Container Registry name"
+}
+
+variable "web_app_name" {
+  type        = string
+  description = "Azure Container App name"
+}
+
+variable "key_vault_name" {
+  type        = string
+  description = "Key Vault name"
+}
+
+variable "email_api_key" {
+  type        = string
+  description = "Email API Key to store in Key Vault"
+  sensitive   = true
+}
+
+variable "acr_admin_username" {
+  type        = string
+  description = "ACR admin username"
+}
+
+variable "acr_admin_password" {
+  type        = string
+  description = "ACR admin password"
+  sensitive   = true
+}
+
+variable "container_image" {
+  type        = string
+  description = "Docker image name"
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "App Service Plan name"
+}
+
+#############################
+# 🔧 Infrastructure         #
+#############################
 
 # 🔷 ACR
 resource "azurerm_container_registry" "acr" {
