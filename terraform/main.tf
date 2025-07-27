edbbd2009373636b49f7dfb1f18d8f8c7c6669e9
yousmakhayyam <yousmakhayyam@gmail.com>
2025-07-27 15:09:17 +0500

    rerun change to main.tf commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,16 +16,47 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 # 🌍 Variables
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "acr_name" { type = string }
-variable "web_app_name" { type = string }
-variable "key_vault_name" { type = string }
-variable "email_api_key" { type = string, sensitive = true }
-variable "acr_admin_username" { type = string }
-variable "acr_admin_password" { type = string, sensitive = true }
-variable "container_image" { type = string }
-variable "app_service_plan_name" { type = string }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "acr_name" {
+  type = string
+}
+
+variable "web_app_name" {
+  type = string
+}
+
+variable "key_vault_name" {
+  type = string
+}
+
+variable "email_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "acr_admin_username" {
+  type = string
+}
+
+variable "acr_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
 
 # 📦 ACR
 resource "azurerm_container_registry" "acr" {
@@ -38,15 +69,14 @@ resource "azurerm_container_registry" "acr" {
 
 # 🔐 Key Vault
 resource "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-  purge_protection_enabled = false
+  name                        = var.key_vault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
 }
 
-# 🛡 Terraform SPN access to Key Vault
 resource "azurerm_key_vault_access_policy" "terraform_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
@@ -55,7 +85,6 @@ resource "azurerm_key_vault_access_policy" "terraform_policy" {
   secret_permissions = ["Get", "Set", "List"]
 }
 
-# 🔐 Store email API key in Key Vault
 resource "azurerm_key_vault_secret" "api_key" {
   name         = "EMAIL-API-KEY"
   value        = var.email_api_key
@@ -72,9 +101,9 @@ resource "azurerm_container_app_environment" "env" {
 
 # 🚀 Container App
 resource "azurerm_container_app" "app" {
-  name                         = var.web_app_name
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  resource_group_name          = var.resource_group_name
+  name                          = var.web_app_name
+  container_app_environment_id  = azurerm_container_app_environment.env.id
+  resource_group_name           = var.resource_group_name
 
   revision_mode = "Single"
 
@@ -120,7 +149,7 @@ resource "azurerm_container_app" "app" {
   depends_on = [azurerm_key_vault_secret.api_key]
 }
 
-# 🔐 Grant Web App access to Key Vault
+# 🔐 App Access to Key Vault
 resource "azurerm_key_vault_access_policy" "app_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
@@ -128,7 +157,6 @@ resource "azurerm_key_vault_access_policy" "app_policy" {
 
   secret_permissions = ["Get"]
 }
-
 output "container_app_url" {
   value       = azurerm_container_app.app.latest_revision_fqdn
   description = "Public URL of the container app"
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "container_app_url" {
-  value       = azurerm_container_app.app.latest_revision_fqdn
-  description = "Public URL of the container app"
-}
