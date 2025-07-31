3fc979ac135fb1d295e090e7380f9b89ef2a37dc
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-01 00:48:35 +0500

    its too much now

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,16 +36,10 @@ variable "email_api_key" {
 }
 variable "container_image" { type = string }
 
-resource "azurerm_container_app_environment" "env" {
-  name                = "contact-env"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = var.resource_group_name
   location            = var.location
+  resource_group_name = var.resource_group_name
   sku                 = "Basic"
   admin_enabled       = false
 }
@@ -71,6 +65,12 @@ resource "azurerm_key_vault_secret" "api_key" {
   value        = var.email_api_key
   key_vault_id = azurerm_key_vault.kv.id
   depends_on   = [azurerm_key_vault_access_policy.terraform_policy]
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                = "contact-env"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_container_app" "app" {
