f923023ea5ec429a2a03f7651d2ce74c94934eff
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-02 22:34:34 +0500

    addes patch.tf

--- terraform/patch.tf
+++ terraform/patch.tf
@@ -1,30 +1,42 @@
 resource "azapi_update_resource" "patch_image_and_secret" {
-  type        = "Microsoft.App/containerApps@2023-05-01"
-  resource_id = azurerm_container_app.app.id
+  type      = "Microsoft.App/containerApps@2023-05-01"
+  name      = azurerm_container_app.app.name
+  parent_id = azurerm_container_app.app.id
 
   body = jsonencode({
     properties = {
       template = {
-        containers = [{
-          name  = "placeholder"
-          image = "${azurerm_container_registry.acr.login_server}/${var.container_image}:latest"
-          env = [{
-            name  = "EMAIL_API_KEY"
-            secretRef = "EMAIL-API-KEY"
-          }]
-        }]
-        secrets = [{
-          name = "EMAIL-API-KEY"
-          keyVaultUrl = azurerm_key_vault_secret.api_key.id
-        }]
+        containers = [
+          {
+            name  = "placeholder"
+            image = "${azurerm_container_registry.acr.login_server}/${var.container_image}:latest"
+            env = [
+              {
+                name      = "EMAIL-API-KEY"
+                secretRef = "EMAIL-API-KEY"
+              }
+            ]
+          }
+        ]
+        secrets = [
+          {
+            name         = "EMAIL-API-KEY"
+            identity     = azurerm_user_assigned_identity.ua_identity.id
+            keyVaultUrl  = azurerm_key_vault_secret.api_key.id
+          }
+        ]
+      }
+      configuration = {
+        secrets = [
+          {
+            name         = "EMAIL-API-KEY"
+            identity     = azurerm_user_assigned_identity.ua_identity.id
+            keyVaultUrl  = azurerm_key_vault_secret.api_key.id
+          }
+        ]
       }
     }
   })
 
-  depends_on = [
-    azurerm_container_app.app,
-    azurerm_key_vault_secret.api_key,
-    azurerm_key_vault_access_policy.app_policy,
-    azurerm_role_assignment.acr_pull
-  ]
+  response_export_values = ["properties.configuration"]
 }
