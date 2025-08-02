8151976e03aef4629deb13db50ecca6139ee7475
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-02 19:47:49 +0500

    fix in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,10 +155,12 @@ resource "azapi_update_resource" "patch_container_app" {
           keyVaultUrl = azurerm_key_vault_secret.api_key.versionless_id
         }]
         activeRevisionsMode = "Single"
-        registries = [{
-          server   = azurerm_container_registry.acr.login_server
-          identity = "SystemAssigned"
-        }]
+        registries = [  # ✅ FIXED block
+          {
+            server   = azurerm_container_registry.acr.login_server
+            identity = azurerm_container_app.app.identity[0].principal_id
+          }
+        ]
       }
       template = {
         containers = [{
