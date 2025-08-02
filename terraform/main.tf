11a1bafdc1452c55a08d34a7251e110e4e23e224
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-02 23:05:53 +0500

    updated  patch.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ resource "azurerm_key_vault_access_policy" "terraform_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
-  secret_permissions = ["Get", "Set", "List"]
+  secret_permissions =  ["Get", "Set", "List", "Delete"]
 }
 
 resource "azurerm_key_vault_secret" "api_key" {
