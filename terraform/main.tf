54cf17089aba012347ffbebc534c9a264ca97d1e
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-03 19:16:36 +0500

    update both files

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.96.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.12.0"
+    }
     time = {
       source  = "hashicorp/time"
       version = "~> 0.9.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
     }
   }
 
