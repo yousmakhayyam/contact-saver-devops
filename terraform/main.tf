596ae446a0e90e01971455f565d85fa103f25808
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-04 21:34:44 +0500

    permission issue solved

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,6 @@ variable "web_app_name"        { type = string }
 variable "key_vault_name"      { type = string }
 variable "container_image"     { type = string }
 
-# Azure Container Registry
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
@@ -48,7 +47,6 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-# Azure Key Vault
 resource "azurerm_key_vault" "kv" {
   name                        = var.key_vault_name
   location                    = var.location
@@ -66,19 +64,17 @@ resource "azurerm_key_vault_access_policy" "terraform_policy" {
   secret_permissions = ["Get", "Set", "List"]
 }
 
-# Container App Environment
 resource "azurerm_container_app_environment" "env" {
   name                = "contact-env"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
-# Container App
 resource "azurerm_container_app" "app" {
-  name                         = var.web_app_name
+  name                          = var.web_app_name
   container_app_environment_id = azurerm_container_app_environment.env.id
-  resource_group_name          = var.resource_group_name
-  revision_mode                = "Single"
+  resource_group_name           = var.resource_group_name
+  revision_mode                 = "Single"
 
   identity {
     type = "SystemAssigned"
@@ -116,7 +112,6 @@ resource "azurerm_container_app" "app" {
   }
 }
 
-# ACR Pull Permission
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
@@ -124,13 +119,11 @@ resource "azurerm_role_assignment" "acr_pull" {
   depends_on           = [azurerm_container_app.app]
 }
 
-# Wait for identity and role to propagate
 resource "time_sleep" "wait_for_identity" {
   depends_on      = [azurerm_role_assignment.acr_pull]
   create_duration = "30s"
 }
 
-# Patch Container App with image and registry config + KV secret reference
 resource "azapi_update_resource" "patch_container_image" {
   type        = "Microsoft.App/containerApps@2023-05-01"
   resource_id = azurerm_container_app.app.id
@@ -139,20 +132,20 @@ resource "azapi_update_resource" "patch_container_image" {
   body = jsonencode({
     properties = {
       configuration = {
-        registries = [ {
+        registries = [{
           server   = azurerm_container_registry.acr.login_server
           identity = "SystemAssigned"
         }]
       }
       template = {
-        containers = [ {
+        containers = [{
           name  = "backend"
           image = "${var.container_image}:latest"
           resources = {
             cpu    = 0.5
             memory = "1.0Gi"
           }
-          env = [ {
+          env = [{
             name = "EMAIL_API_KEY"
             secrets = {
               keyVaultUrl = "${azurerm_key_vault.kv.vault_uri}secrets/email-api-key"
@@ -164,7 +157,6 @@ resource "azapi_update_resource" "patch_container_image" {
   })
 }
 
-# Output URL
 output "container_app_url" {
   value = azurerm_container_app.app.latest_revision_fqdn
 }
