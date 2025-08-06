f928e162dd9b1460bb0fe82040bc28e34023ac70
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-06 20:02:04 +0500

    first testing part 1 of simple web

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,6 @@ resource "azurerm_container_app" "app" {
   name                         = "myproject-webapp"
   container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
 
   revision_mode = "Single"
 
