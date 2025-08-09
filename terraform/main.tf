d80f338953c2b563bac630eba4589668e903a449
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-09 15:06:02 +0500

    Now add key vault

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">=1.4.0"
+
   backend "azurerm" {
     resource_group_name  = "yousma-rg"
     storage_account_name = "yousmastorage"
@@ -9,105 +11,85 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.75.0"
+      version = "~>3.107"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "adc9f320-e56e-45b1-845e-c73484745fc8"
 }
 
-data "azurerm_client_config" "current" {}
-
-# ✅ Resource group ko data source me convert kiya
+# Reference existing resource group (prevents deletion issue)
 data "azurerm_resource_group" "rg" {
   name = "yousma-rg"
 }
 
+# Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-  name                = "myprojectacr1234"
+  name                = "contactsaveracr1234"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
 }
 
-resource "azurerm_user_assigned_identity" "acr_pull_identity" {
-  name                = "acr-pull-identity"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-resource "azurerm_role_assignment" "acr_pull_role" {
-  principal_id         = azurerm_user_assigned_identity.acr_pull_identity.principal_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
-}
-
-resource "azurerm_container_app_environment" "env" {
-  name                = "myproject-env"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
+# Key Vault
 resource "azurerm_key_vault" "kv" {
-  name                = "yousma-kv"
+  name                = "yousmakv"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 }
 
+data "azurerm_client_config" "current" {}
+
+# Access policy for pipeline service principal
+resource "azurerm_key_vault_access_policy" "pipeline_sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = var.pipeline_sp_object_id
+
+  secret_permissions = ["Get", "List", "Set"]
+}
+
+# Store secret in Key Vault (lowercase to avoid naming issues)
 resource "azurerm_key_vault_secret" "email_api_key" {
-  name         = "EMAIL-API-KEY"
+  name         = "email-api-key"
   value        = var.email_api_key
   key_vault_id = azurerm_key_vault.kv.id
 }
 
-resource "azurerm_key_vault_access_policy" "acr_identity_policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.acr_pull_identity.principal_id
-
-  secret_permissions = [
-    "Get",
-    "List"
-  ]
+# Container App Environment
+resource "azurerm_container_app_environment" "cae" {
+  name                = "contactsaver-env"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+# Container App with ACR pull & Key Vault secret
 resource "azurerm_container_app" "app" {
-  name                         = "myproject-webapp"
-  container_app_environment_id = azurerm_container_app_environment.env.id
+  name                         = "contactsaver-app"
+  container_app_environment_id = azurerm_container_app_environment.cae.id
   resource_group_name          = data.azurerm_resource_group.rg.name
   revision_mode                = "Single"
 
   identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.acr_pull_identity.id]
+    type = "SystemAssigned"
   }
 
-  ingress {
-    external_enabled = true
-    target_port      = 80
-
-    traffic_weight {
-      latest_revision = true
-      percentage      = 100
-    }
+  registry {
+    server   = azurerm_container_registry.acr.login_server
+    identity = "SystemAssigned"
   }
 
   template {
     container {
-      name   = "myapp"
-      image  = "${azurerm_container_registry.acr.login_server}/moodly:latest"
+      name   = "contactsaver-container"
+      image  = "${azurerm_container_registry.acr.login_server}/contactsaver:latest"
       cpu    = 0.5
-      memory = "1.0Gi"
-
-      env {
-        name        = "WEBSITES_PORT"
-        value       = "80"
-      }
+      memory = "1Gi"
 
       env {
         name        = "EMAIL_API_KEY"
@@ -117,33 +99,14 @@ resource "azurerm_container_app" "app" {
   }
 
   secret {
-    name                = "email-api-key"
-    key_vault_secret_id = azurerm_key_vault_secret.email_api_key.id
-    identity            = azurerm_user_assigned_identity.acr_pull_identity.id
+    name  = "email-api-key"
+    value = azurerm_key_vault_secret.email_api_key.value
   }
-
-  registry {
-    server   = azurerm_container_registry.acr.login_server
-    identity = azurerm_user_assigned_identity.acr_pull_identity.id
-  }
-
-  tags = {
-    environment = "dev"
-  }
-
-  depends_on = [
-    azurerm_role_assignment.acr_pull_role,
-    azurerm_key_vault_access_policy.acr_identity_policy
-  ]
-}
-output "app_url" {
-  value       = "https://${azurerm_container_app.app.latest_revision_fqdn}"
-  description = "Public URL of the deployed Moodly app"
 }
 
-variable "email_api_key" {
-  description = "Secret value to store in Key Vault (set this in pipeline as a secret variable)."
-  type        = string
-  sensitive   = true
-  default     = "change-me-please"
+# Give Container App identity ACR pull permission
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_container_app.app.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
 }
