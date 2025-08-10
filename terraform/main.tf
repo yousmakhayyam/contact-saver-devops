2f343d97455ca5956d0af87d05711802760dd73b
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-10 19:57:23 +0500

    Now add key vault

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,9 +32,6 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name      = azurerm_resource_group.rg.name
   tenant_id                = data.azurerm_client_config.current.tenant_id
   sku_name                 = "standard"
-
-  soft_delete_enabled      = true
-  purge_protection_enabled = false
 }
 
 # Changed here: Use Terraform variable for secret value instead of hardcoded string
