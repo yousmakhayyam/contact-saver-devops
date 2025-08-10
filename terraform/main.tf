3b827da93b162b6650bc0026c40aa85b81168cb7
yousmakhayyam <yousmakhayyam@gmail.com>
2025-08-10 20:07:59 +0500

    Now add key vault

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,8 @@ resource "azurerm_key_vault_secret" "db_password" {
 resource "azurerm_key_vault_access_policy" "app_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id = azurerm_container_app.app.identity[0].principal_id
+  object_id = azurerm_user_assigned_identity.acr_pull_identity.principal_id
+
 
 
   secret_permissions = [
